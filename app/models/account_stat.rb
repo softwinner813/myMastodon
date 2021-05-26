f3492c2712fbe2a07586b88399ec85d8c416abed
softwinner813 <winner813@protonmail.com>
2021-05-25 19:37:27 -0700

    2021.5.25 Init Source
    Init Source

---
+++ app/models/account_stat.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+# == Schema Information
+#
+# Table name: account_stats
+#
+#  id              :bigint(8)        not null, primary key
+#  account_id      :bigint(8)        not null
+#  statuses_count  :bigint(8)        default(0), not null
+#  following_count :bigint(8)        default(0), not null
+#  followers_count :bigint(8)        default(0), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  last_status_at  :datetime
+#  lock_version    :integer          default(0), not null
+#
+
+class AccountStat < ApplicationRecord
+  belongs_to :account, inverse_of: :account_stat
+
+  update_index('accounts#account', :account)
+end
